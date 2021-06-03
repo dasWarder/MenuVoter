73132cab4a447dc0b4b7cc422814861789beacfd
dasWarder <Al.94_94@mail.ru>
2021-06-03 09:01:53 +0300

    Fix dockerfile and docker-compose, add start shell script and add application.yaml properties for databases connections

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ ARG FILE_NAME=menu-common/service/target/*.jar
 RUN apk add openjdk11
 COPY $FILE_NAME /app.jar
 
-ENTRYPOINT["java", "-jar", "/app.jar"]
+ENTRYPOINT ["java", "-jar", "/app.jar"]
--- menu-common/web/src/main/java/com/example/web/WebApplication.java
+++ menu-common/web/src/main/java/com/example/web/WebApplication.java
@@ -2,8 +2,11 @@ package com.example.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+
+
+@SpringBootApplication(exclude = MongoAutoConfiguration.class)
 public class WebApplication {
 
     public static void main(String[] args) {
